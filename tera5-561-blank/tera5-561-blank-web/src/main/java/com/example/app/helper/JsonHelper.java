654c790cc4efb593b60b8cc0d599857918b4bc94
yonahaty <start.zerozaki@gmail.com>
2021-04-04 21:02:11 +0900

    json mapping sample add

--- tera5-561-blank/tera5-561-blank-web/src/main/java/com/example/app/helper/JsonHelper.java
+++ tera5-561-blank/tera5-561-blank-web/src/main/java/com/example/app/helper/JsonHelper.java
@@ -3,12 +3,57 @@
  */
 package com.example.app.helper;
 
+import java.util.List;
+import java.util.Map;
+
+import com.example.app.bean.json.request.JsonSampleBean;
+import com.example.app.bean.json.request.JsonSampleListBean;
+import com.example.app.bean.json.request.JsonSampleMapBean;
+
 /**
  * JSONヘルパー。
  *
  * @author start
  */
-interface JsonHelper {
+public interface JsonHelper {
 
-    Object mappingObject(String json);
+	/**
+	 * JSONをオブジェクト型にマッピングする。
+	 *
+	 * @param json JSON文字列
+	 * @return オブジェクト型
+	 */
+	Object mappingObject(String json);
+
+	/**
+	 * JSONをオブジェクト型（リスト含む）にマッピングする。
+	 *
+	 * @param json JSON文字列
+	 * @return オブジェクト型（リスト含む）
+	 */
+	JsonSampleListBean mappingListPropertyObject(String json);
+
+	/**
+	 * JSONをオブジェクト型（マップ含む）にマッピングする。
+	 *
+	 * @param json JSON文字列
+	 * @return オブジェクト型（マップ含む）
+	 */
+	JsonSampleMapBean mappingMapPropertyObject(String json);
+
+	/**
+	 * JSONをリスト<オブジェクト>型にマッピングする。
+	 *
+	 * @param json JSON文字列
+	 * @return リスト<オブジェクト>型
+	 */
+	List<JsonSampleBean> mappingObjectList(String json);
+
+	/**
+	 * JSONをマップ<String, オブジェクト>型にマッピングする。
+	 *
+	 * @param json JSON文字列
+	 * @return マップ<String, オブジェクト>型
+	 */
+	Map<String, JsonSampleBean> mappingObjecMap(String json);
 }
--- tera5-561-blank/tera5-561-blank-web/src/main/java/com/example/app/helper/JsonHelperImpl.java
+++ tera5-561-blank/tera5-561-blank-web/src/main/java/com/example/app/helper/JsonHelperImpl.java
@@ -3,8 +3,17 @@
  */
 package com.example.app.helper;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
 import com.example.app.bean.json.request.JsonSampleBean;
+import com.example.app.bean.json.request.JsonSampleListBean;
+import com.example.app.bean.json.request.JsonSampleMapBean;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -13,28 +22,134 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  *
  * @author start
  */
+@Component
 public class JsonHelperImpl implements JsonHelper {
 
-    /**
-     * チェックを実行する。
-     */
-    @Override
-    public JsonSampleBean mappingObject(String json) {
+	/**
+	 * JSONをオブジェクト型にマッピングする。
+	 *
+	 * @param json JSON文字列
+	 * @return オブジェクト型
+	 */
+	@Override
+	public JsonSampleBean mappingObject(String json) {
 
-        ObjectMapper mapper = new ObjectMapper();
-        JsonSampleBean bean = null;
+		ObjectMapper mapper = new ObjectMapper();
+		JsonSampleBean bean = null;
 
-        try {
-        	bean = mapper.readValue(json, JsonSampleBean.class);
-        } catch (JsonMappingException e) {
-            // TODO 自動生成された catch ブロック
-            e.printStackTrace();
-        } catch (JsonProcessingException e) {
-            // TODO 自動生成された catch ブロック
-            e.printStackTrace();
-        }
+		try {
+			bean = mapper.readValue(json, JsonSampleBean.class);
+		} catch (JsonMappingException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 
-        return bean;
-    }
+		return bean;
+	}
 
+	/**
+	 * JSONをオブジェクト型（リスト含む）にマッピングする。
+	 *
+	 * @param json JSON文字列
+	 * @return オブジェクト型（リスト含む）
+	 */
+	@Override
+	public JsonSampleListBean mappingListPropertyObject(String json) {
+
+		ObjectMapper mapper = new ObjectMapper();
+		JsonSampleListBean bean = null;
+
+		try {
+			bean = mapper.readValue(json, JsonSampleListBean.class);
+		} catch (JsonMappingException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		return bean;
+	}
+
+	/**
+	 * JSONをオブジェクト型（マップ含む）にマッピングする。
+	 *
+	 * @param json JSON文字列
+	 * @return オブジェクト型（マップ含む）
+	 */
+	@Override
+	public JsonSampleMapBean mappingMapPropertyObject(String json) {
+
+		ObjectMapper mapper = new ObjectMapper();
+		JsonSampleMapBean bean = null;
+
+		try {
+			bean = mapper.readValue(json, JsonSampleMapBean.class);
+		} catch (JsonMappingException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		return bean;
+	}
+
+	/**
+	 * JSONをリスト<オブジェクト>型にマッピングする。
+	 *
+	 * @param json JSON文字列
+	 * @return リスト<オブジェクト>型
+	 */
+	@Override
+	public List<JsonSampleBean> mappingObjectList(String json) {
+
+		ObjectMapper mapper = new ObjectMapper();
+		List<JsonSampleBean> beanList = null;
+
+		try {
+			beanList = mapper.readValue(json, new TypeReference<List<JsonSampleBean>>() {
+			});
+		} catch (JsonMappingException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		return beanList;
+	}
+
+	/**
+	 * JSONをマップ<String, オブジェクト>型にマッピングする。
+	 *
+	 * @param json JSON文字列
+	 * @return マップ<String, オブジェクト>型
+	 */
+	@Override
+	public Map<String, JsonSampleBean> mappingObjecMap(String json) {
+
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, JsonSampleBean> map = null;
+
+		try {
+			map = mapper.readValue(json, new TypeReference<LinkedHashMap<String, JsonSampleBean>>() {
+			});
+
+		} catch (JsonMappingException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		return map;
+	}
 }
