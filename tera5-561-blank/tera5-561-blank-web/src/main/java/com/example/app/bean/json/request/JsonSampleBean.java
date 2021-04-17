f0aac353e1bd5904feac1e0304915034984711d2
yonahaty <start.zerozaki@gmail.com>
2021-04-17 20:51:07 +0900

    JsonProperty Sample add

--- tera5-561-blank/tera5-561-blank-web/src/main/java/com/example/app/bean/json/request/JsonSampleBean.java
+++ tera5-561-blank/tera5-561-blank-web/src/main/java/com/example/app/bean/json/request/JsonSampleBean.java
@@ -1,5 +1,7 @@
 package com.example.app.bean.json.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,11 +15,13 @@ public class JsonSampleBean {
 	/** ID */
 	@Getter
 	@Setter
+	@JsonProperty("ID")
 	private String id;
 
 	/** 名称 */
 	@Getter
 	@Setter
+	@JsonProperty("name_kana")
 	private String name;
 
 }
